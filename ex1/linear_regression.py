e4b575e6f965ec1e261a430721a0ca5eca4cdb07
mvpzhao@mac <zhaochuanyun@qq.com>
2018-08-16 16:19:59 +0800

    plot data
    Change-Id: Ibc68993e5f29cb95bee224c3f4652e547ff5cdfa

--- ex1/linear_regression.py
+++ ex1/linear_regression.py
@@ -1,4 +1,8 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('ex1data1.txt')
 
 x = np.array([
     [1,2,3],
@@ -12,3 +16,6 @@ y = [
 ]
 
 print(np.dot(x, y))
+
+def computeCostFunction(X, y, theta):
+    np.dot(X, theta)
